7b2f0c74cf3d8b6270182a2bc2e0ca3e3e861d4e
lizushka <elizavetavostokova@MacBook-Pro-Lizaveta.local>
2017-03-19 13:02:05 +0300

    + navigation panel + theme

--- ui.R
+++ ui.R
@@ -1,18 +1,24 @@
 library(shiny)
+library(shinythemes)
 library(DT)
 
 sup <- read.csv("85-173.csv",sep="\t",header=TRUE)
 
-fluidPage(
-  titlePanel("Шугнанско-русский словарь"),
+navbarPage(theme = shinytheme("readable"),
+  "Шугнанско-русский словарь",
+  tabPanel("Dictionary",
+    titlePanel("Шугнанско-русский словарь"),
+    p("Специальные символы:"),
+    sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput('show_vars', 'Please, select information to show:',
+                          names(sup), selected = names(sup)), width = 2
+      ),
+      mainPanel(
+        DT::dataTableOutput("filtred_dic"))
+  )),
+  tabPanel("About",
   p("Этот словарь представляет собой оцифрованную версию словаря И.И.Зарубина."),
-  em("Зарубин, И. И. (1960). Шугнанские тексты и словарь. Изд-во Академии наук СССР."),
-  p("Специальные символы:"),
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput('show_vars', 'Please, select information to show:',
-                         names(sup), selected = names(sup)), width = 2
-    ),
-    mainPanel(
-      DT::dataTableOutput("filtred_dic"))
-))
+  p(em("Зарубин, И. И. (1960). Шугнанские тексты и словарь. Изд-во Академии наук СССР."))
+  )
+)
