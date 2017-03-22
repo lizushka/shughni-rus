5b2e3e51a83dabedeeebf49dbde9b83b589caf87
lizushka <belka.liza@gmail.com>
2017-03-22 20:46:35 +0300

    Update server.R
    page length in morpheme dict

--- server.R
+++ server.R
@@ -8,5 +8,5 @@ function(input, output) {
                   options = list(autoWidth = TRUE, columnDefs = list(list(width = '400px', targets = c(1, 3))),
                     lengthMenu = list(c(10, 30, 50, 100, -1), list('10', '30', '50', '100', 'All')), pageLength = 10))
   })
-  output$morph <- DT::renderDataTable(DT::datatable(read.csv("morph_2203.csv",sep="\t",header=TRUE)), options = list(pageLength = 20))
+  output$morph <- DT::renderDataTable(DT::datatable(read.csv("morph_2203.csv",sep="\t",header=TRUE)), options = list(lengthMenu(pageLength = 20)))
 }
