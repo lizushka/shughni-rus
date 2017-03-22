449101ca3b68232760c0ea86d63b50184f658bbc
lizushka <elizavetavostokova@MacBook-Pro-Lizaveta.local>
2017-03-22 20:38:27 +0300

    + morpheme dictionary + list of special symbols

--- server.R
+++ server.R
@@ -2,9 +2,11 @@ library(shiny)
 library(DT)
 
 function(input, output) {
-  sup <- read.csv("85-173.csv",sep="\t",header=TRUE)
+  dic <- read.csv("85-173.csv",sep="\t",header=TRUE)
   output$filtred_dic <- DT::renderDataTable({
-    DT::datatable(sup[, input$show_vars, drop = FALSE],
-                  options = list(lengthMenu = list(c(10, 30, 50, 100, -1), list('10', '30', '50', '100', 'All')), pageLength = 10))
+    DT::datatable(dic[, input$show_vars, drop = FALSE],
+                  options = list(autoWidth = TRUE, columnDefs = list(list(width = '400px', targets = c(1, 3))),
+                    lengthMenu = list(c(10, 30, 50, 100, -1), list('10', '30', '50', '100', 'All')), pageLength = 10))
   })
+  output$morph <- DT::renderDataTable(DT::datatable(read.csv("morph_2203.csv",sep="\t",header=TRUE)), options = list(pageLength = 20))
 }
--- ui.R
+++ ui.R
@@ -2,23 +2,30 @@ library(shiny)
 library(shinythemes)
 library(DT)
 
-sup <- read.csv("85-173.csv",sep="\t",header=TRUE)
+dic <- read.csv("85-173.csv",sep="\t",header=TRUE)
 
 navbarPage(theme = shinytheme("readable"),
-  "Шугнанско-русский словарь",
+  "Shughni Project",
   tabPanel("Dictionary",
     titlePanel("Шугнанско-русский словарь"),
-    p("Специальные символы:"),
+    p("Специальные символы: á ā ā́ | é ē ḗ ẹ ẹ́ ȇ ȇ́ | í ī ī́ | ó ō ṓ | ú ū ū́ ů ů́ | ɣ ɣ̌ x̌ š č ž ʒ ǰ ð θ"),
     sidebarLayout(
       sidebarPanel(
         checkboxGroupInput('show_vars', 'Please, select information to show:',
-                          names(sup), selected = names(sup)), width = 2
+                          names(dic), selected = names(dic)), width = 2
       ),
       mainPanel(
         DT::dataTableOutput("filtred_dic"))
   )),
+  tabPanel("Morphemes",
+    titlePanel("Inflectional and Derivational Affixes"),
+    p("Специальные символы: á ā ā́ | é ē ḗ ẹ ẹ́ ȇ ȇ́ | í ī ī́ | ó ō ṓ | ú ū ū́ ů ů́ | ɣ ɣ̌ x̌ š č ž ʒ ǰ ð θ"),
+    mainPanel(DT::dataTableOutput("morph")
+  )),
   tabPanel("About",
   p("Этот словарь представляет собой оцифрованную версию словаря И.И.Зарубина."),
-  p(em("Зарубин, И. И. (1960). Шугнанские тексты и словарь. Изд-во Академии наук СССР."))
+  p(em("Зарубин, И. И. (1960). Шугнанские тексты и словарь. Изд-во Академии наук СССР.")),
+  p("Работа была проведена в рамках научно-исследовательского семинара студентами магистратуры НИУ ВШЭ."),
+  p("Со всеми вопросами и комментариями пишите на почту shughni.project@gmail.com")
   )
 )
