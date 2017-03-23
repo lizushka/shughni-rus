7c17918b74057b72660824e65a597c872abd0e9f
lizushka <elizavetavostokova@MacBook-Pro-Lizaveta.local>
2017-03-23 03:23:56 +0300

    Modified search
    + hidden column with simplified orthography

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ function(input, output) {
     DT::datatable(read.csv("85-173.csv",sep="\t",header=TRUE)[, input$show_vars, drop = FALSE],
                   options = list(
                     autoWidth = TRUE,
-                    columnDefs = list(list(width = '600px', targets = c(-1,-2))),
+                    columnDefs = list(list(width = '600px', targets = c(-1,-2)),list(visible=FALSE, targets=c(2))),
                     lengthMenu = list(c(10, 30, 50, 100, -1), list('10', '30', '50', '100', 'All')),
                     pageLength = 10,
                     search = list(regex = TRUE),
