3f61615ce792475254f1bb7f8bacf2e821eab1fb
lizushka <belka.liza@gmail.com>
2018-03-21 11:02:14 +0300

    added part of Karamshoev

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(DT)
 
 function(input, output) {
   output$filtred_dic <- DT::renderDataTable({
-    DT::datatable(read.csv("85-192_225-243.csv",sep="\t",header=TRUE)[, input$show_vars, drop = FALSE],
+    DT::datatable(read.csv("85-288.csv",sep="\t",header=TRUE)[, input$show_vars, drop = FALSE],
                   options = list(
                     autoWidth = TRUE,
                     columnDefs = list(list(width = '600px', targets = c(-1,-2)),list(visible=FALSE, targets=c(2))),
@@ -12,11 +12,22 @@ function(input, output) {
                     search = list(regex = TRUE),
                     searchHighlight = TRUE))
   })
-  output$morph <- DT::renderDataTable({
-    DT::datatable(read.csv("morphology_1804.csv",sep=",",header=TRUE))},
+  output$karam4 <- DT::renderDataTable({
+    DT::datatable(read.csv("karam_part1.csv",sep="\t",header=TRUE),
     options = list(
       autoWidth = TRUE,
-      columnDefs = list(list(width = '30%', targets = "5"))))
+      columnDefs = list(list(width = '600px', targets = "2")),
+      lengthMenu = list(c(10, 30, 50, 100, -1), list('10', '30', '50', '100', 'All')),
+      pageLength = 50,
+      search = list(regex = TRUE),
+      searchHighlight = TRUE))
+  })
+  output$morph <- DT::renderDataTable({
+    DT::datatable(read.csv("morphology.csv",sep=",",header=TRUE),
+    options = list(
+      autoWidth = TRUE,
+      columnDefs = list(list(width = '60%', targets = "5"))))
 ##    ,
 ##    options = list(lengthMenu(pageLength = 20)))
+  })
 }
